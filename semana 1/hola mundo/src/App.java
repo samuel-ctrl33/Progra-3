abff0790e5218826867afa8be9131c171902fb92
Samuel~ <samuel>
2025-09-16 19:29:11 +0200

    variable

--- semana 1/hola mundo/src/App.java
+++ semana 1/hola mundo/src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
+        int a = 33;
+        System.out.println("Hello, World!");
+        a = 44;
         System.out.println("Hello, World!");
 
-        System.out.println("Hello, World!");
-        
     }
 }
