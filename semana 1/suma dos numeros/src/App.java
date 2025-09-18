00e55bbad30e581a1887567e94519a84f69bc1b3
samuel-ctrl33 <samuel4@usal.es>
2025-09-18 12:09:58 +0200

    suma dos numeros

---
+++ semana 1/suma dos numeros/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+    public class App {
+    public static void main(String[] args) throws Exception {
+        // Pedir dos numeros al usuario con Scanner y mostrar la suma de ambos
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int num1 = sc.nextInt();
+        System.out.println("Introduce otro número: ");
+        int num2 = sc.nextInt();
+        System.out.printf("La suma de ambos números es: %d\n", num1 + num2);
+        sc.close();
+    }
+}
