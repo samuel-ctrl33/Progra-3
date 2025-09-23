2f774c3e5d3c356a70020b19f4d0ca0ff7d2a5cc
alumno <alumno@usal.es>
2025-09-23 19:11:01 +0200

    manifest

---
+++ semana 2/ejemploJarManual/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+//Path lo sabe la terminal, para ejecutar un jar se realiza java -jar archivo.jar
