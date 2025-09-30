14777fb2331b6d360f03c9eb8e78d654bfca1dd3
alumno <alumno@usal.es>
2025-09-30 18:31:18 +0200

    importar biblioteca

---
+++ semana 3/ejercicioEsdia/src/App.java
@@ -0,0 +1,10 @@
+import com.coti.tools.Esdia;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        double a = Esdia.readDouble("Dame un double");
+        double b = Esdia.readDouble("Dame un double");
+    }
+}
