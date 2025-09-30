81649f3dc65017b0a58714252de674dacc07524a
alumno <alumno@usal.es>
2025-09-30 18:55:25 +0200

    semana 3

--- semana 3/ejercicioEsdia/src/App.java
+++ semana 3/ejercicioEsdia/src/App.java
@@ -4,7 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        double a = Esdia.readDouble("Dame un double");
-        double b = Esdia.readDouble("Dame un double");
+        double a = Esdia.readDouble("Dame un double ");
+        double b = Esdia.readDouble("Dame un double ");
+        System.out.printf("La suma de %f y %f es %f\n", a, b, a + b);
     }
 }
---
+++ semana 3/ejercicioEsdia/src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    
+    String name = "noname"; //clases en mayúsculas
+
+}
