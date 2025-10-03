65a741a9bbbd80cd9f24106b918d49021393a6f1
samuel-ctrl33 <samuel4@usal.es>
2025-10-03 13:36:01 +0200

    ejercicio extension 1

---
+++ semana 3/ejercicio 1 extensión/src/Tipo.java
@@ -0,0 +1,18 @@
+public class Tipo {
+    float datofloat;
+
+    public double getDatoDouble() {
+        // TODO: implement method logic
+        return 0.0;
+    }
+
+    public float getDatoFloat() {
+        // TODO: implement method logic
+        return 0.0f;
+    }
+
+    public int getDatoInt() {
+        // TODO: implement method logic
+        return 0;
+    }
+}
