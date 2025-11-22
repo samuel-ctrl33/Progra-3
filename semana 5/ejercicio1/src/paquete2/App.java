cf5bd7c8c1ac3aa418074356af9d260145952775
samuel-ctrl33 <samuel4@usal.es>
2025-11-22 11:58:58 +0100

    ejer2

--- semana 5/ejercicio1/src/paquete2/App.java
+++ semana 5/ejercicio1/src/paquete2/App.java
@@ -5,7 +5,8 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         clase1 objClase1 = new clase1() ;
-
+        //los demas no podran ser accedidos, bien porque son default, porque son privados o no tienen herencia
+        objClase1.nombre="Samuel";
 
     }
 }
